4dd9772de5cac6efd9b439afbff96d05bafddf3c
Zecay <110484412+Zecay@users.noreply.github.com>
2026-04-11 21:17:08 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ const { Server } = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: {
-    origin: "*"
-  }
+  cors: { origin: "*" }
 });
 
 const players = {};
@@ -15,25 +13,40 @@ const players = {};
 io.on("connection", (socket) => {
   console.log("Player connected:", socket.id);
 
-  players[socket.id] = { x: 0, y: 0 };
-
-  socket.emit("init", { id: socket.id, players });
-  socket.broadcast.emit("playerJoined", { id: socket.id, x: 0, y: 0 });
+  players[socket.id] = { x: 0, y: 0, lastSeen: Date.now() };
 
   socket.on("move", (data) => {
     if (players[socket.id]) {
       players[socket.id].x = data.x;
       players[socket.id].y = data.y;
+      players[socket.id].lastSeen = Date.now();
+    }
+  });
+
+  socket.on("heartbeat", () => {
+    if (players[socket.id]) {
+      players[socket.id].lastSeen = Date.now();
     }
   });
 
   socket.on("disconnect", () => {
     delete players[socket.id];
     console.log("Player disconnected:", socket.id);
-    io.emit("playerLeft", socket.id);
   });
 });
 
+// Remove ghost players who haven't sent anything in 15 seconds
+setInterval(() => {
+  const now = Date.now();
+  for (const id in players) {
+    if (now - players[id].lastSeen > 15000) {
+      delete players[id];
+      console.log("Removed ghost player:", id);
+    }
+  }
+}, 2000);
+
+// Broadcast full state to all clients every 50ms
 setInterval(() => {
   io.emit("state", players);
 }, 50);
